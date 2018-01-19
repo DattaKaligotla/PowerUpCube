b91d77f1e2209d3a3e06b4324e2b2a6866920f2b
Sreedatta Kaligotla <33785968+SreedattaKaligotla@users.noreply.github.com>
2018-01-19 17:03:27 -0500

    Add files via upload

--- detectingyellow.py
+++ detectingyellow.py
@@ -6,20 +6,29 @@ cap = cv2.VideoCapture(1)
 
 while(True):
     ret, img = cap.read()
+    '''if img == None:
+        continue'''
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
-    #blur = cv2.blur(hsv,(5,5))
-    
+    blur = cv2.blur(hsv,(5,5))
     #lower_range = np.array([59, 45, 45], dtype=np.uint8)
     #upper_range = np.array([80, 100, 100], dtype=np.uint8)
     lower_range = np.array([20, 100, 100], dtype=np.uint8)
     upper_range = np.array([30, 255, 255], dtype=np.uint8)
 
-    mask = cv2.inRange(hsv, lower_range, upper_range)
- 
-    cv2.imshow('mask',mask)
+    mask = cv2.inRange(blur, lower_range, upper_range)
+    thresh = 127
+    maxValue = 255
+
+    ret, thresh = cv2.threshold(mask, 127, 255, 0)
+    im2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
+    cv2.drawContours(mask, contours, -1, (0,255,0), 3)
+
+    
+    cv2.waitKey(0)
     cv2.imshow('image', img)
-    cv2.waitKey(1)
+    cv2.imshow('Objects Detected',mask)
+    cv2.waitKey(0)
 # When everything done, release the capture
-cv2.waitKey(0)
+#cv2.waitKey(1)
 cap.release()
 cv2.destroyAllWindows()
