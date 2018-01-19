1903ce25dd29962974f6e44b1044a689894ebb14
Sreedatta Kaligotla <33785968+SreedattaKaligotla@users.noreply.github.com>
2018-01-19 17:14:16 -0500

    Add files via upload

--- detectingyellow.py
+++ detectingyellow.py
@@ -21,7 +21,7 @@ while(True):
 
     ret, thresh = cv2.threshold(mask, 127, 255, 0)
     im2, contours, hierarchy = cv2.findContours(thresh, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    cv2.drawContours(mask, contours, -1, (0,255,0), 3)
+    cv2.drawContours(img, contours, -1, (0,255,0), 3)
 
     
     cv2.waitKey(0)
